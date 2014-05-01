9aca0ff6b59dba4714c9dcc15eb34fb3efef0a87
Kirsteen Jewell <kirsteenjewell@Kirsteens-iMac.local>
2014-05-01 10:14:19 +0100

    Added test class

---
+++ KJTest.h
@@ -0,0 +1,13 @@
+//
+//  KJTest.h
+//  Git Test
+//
+//  Created by Kirsteen Jewell on 01/05/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KJTest : NSObject
+
+@end
---
+++ KJTest.m
@@ -0,0 +1,13 @@
+//
+//  KJTest.m
+//  Git Test
+//
+//  Created by Kirsteen Jewell on 01/05/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import "KJTest.h"
+
+@implementation KJTest
+
+@end
